c0485b44e3aff30c9cb3e40af76bd1aceef6f6da
Sushil <grayson.9999@gmail.com>
2024-08-06 21:16:37 +1000

    adding useState for Form input section

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,56 @@
 import "./App.css";
+import { Form } from "./components/Form";
 
 function App() {
-  return <>hello there !</>;
+  return (
+    <div className="wrapper pt-5">
+      {/* <!-- title  --> */}
+
+      <div className="container">
+        <h1 className="text-center">Time Wise</h1>
+
+        {/* <!-- form  --> */}
+
+        <Form />
+
+        {/* <!-- tables   --> */}
+        <div className="row mt-5">
+          <div className="col">
+            <h3 className="text-center">Entry List</h3>
+            {/* <!-- Entry List table  --> */}
+            <hr />
+            <table className="table table-striped table-hover border">
+              <tbody id="entryList"></tbody>
+            </table>
+          </div>
+
+          <div className="col bg-info">
+            {/* <!-- Bad List table  --> */}
+
+            <div className="row mt-5">
+              <div className="col">
+                <h3 className="text-center">Bad List</h3>
+
+                <hr />
+                <table className="table table-striped table-hover border">
+                  <tbody id="badList"></tbody>
+                </table>
+
+                <div className="alert alert-success">
+                  You could have saved = <span id="savedHrsElm">0</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <br />
+        <div className="alert alert-success">
+          The total hours allocated = <span id="ttlHrs">0</span> hrs
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Form.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+
+export const Form = () => {
+  const [Form, setForm] = useState({});
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setForm({ ...Form, [name]: value });
+  };
+  console.log(Form);
+  return (
+    <form
+      onSubmit="handleOnSubmit(this)"
+      action="javascript:void(0)"
+      className="border p-5 rounded shadow-lg mt-5"
+    >
+      <div className="row g-2">
+        <div className="col-md-7">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Task"
+            aria-label="First name"
+            name="task"
+            id="task"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="col-md-2">
+          <input
+            type="number"
+            className="form-control"
+            placeholder="40"
+            aria-label="Last name"
+            name="hr"
+            min="1"
+            onChange={handleOnChange}
+          />
+        </div>
+        <div className="col-md-3 d-grid">
+          <button className="btn btn-primary">Add New Task</button>
+        </div>
+      </div>
+    </form>
+  );
+};
