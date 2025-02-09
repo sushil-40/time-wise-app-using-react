7204b365de1171800ae071e28e77442b34f88395
Sushil <grayson.9999@gmail.com>
2025-02-09 13:01:56 +1000

    implementing multiple select in entry list and bad list and also select all features and unselect all features in entry list

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -1,23 +1,66 @@
 /* eslint-disable react/prop-types */
-import React from "react";
+import React, { useState } from "react";
 
 export const Table = ({ taskList, switchTask, handleOnDelete }) => {
+  const [toDelete, setToDelete] = useState([]);
   const entryList = taskList.filter((item) => item.type === "entry");
   const badList = taskList.filter((item) => item.type === "bad");
 
+  const handleOnSelect = (e) => {
+    // console.log(e);
+    const { checked, value } = e.target;
+    console.log(checked, value);
+    if (checked) {
+      if (value === "all-entry") {
+        // get all _ids from entry list
+        const _ids = entryList.map((item) => item._id);
+        //_ids is also array thats why we are spreading below;
+        setToDelete([...toDelete, ..._ids]);
+        return;
+      }
+      setToDelete([...toDelete, value]);
+    } else {
+      if (value === "all-entry") {
+        const _ids = entryList.map((item) => item._id);
+
+        setToDelete(toDelete.filter((_id) => !_ids.includes(_id)));
+        return;
+      }
+      setToDelete(toDelete.filter((_id) => _id != value));
+    }
+  };
+  console.log(toDelete);
   return (
     <div className="row mt-5">
       <div className="col">
         <h3 className="text-center">Entry List</h3>
         {/* <!-- Entry List table  --> */}
         <hr />
+        <input
+          className="form-check-input"
+          type="checkbox"
+          value="all-entry"
+          id="all-entry"
+          onChange={handleOnSelect}
+        />{" "}
+        <label htmlFor="all-entry">Select All</label>
         <table className="table table-striped table-hover border">
           <tbody id="entryList">
             {entryList.map((item, i) => {
               return (
                 <tr key={item?._id}>
                   <td>{i + 1}</td>
-                  <td>{item.task}</td>
+                  <td>
+                    {" "}
+                    <input
+                      className="form-check-input"
+                      type="checkbox"
+                      value={item?._id}
+                      onChange={handleOnSelect}
+                      checked={toDelete.includes(item?._id)}
+                    />{" "}
+                    {item.task}
+                  </td>
                   <td>{item.hr}</td>
                   <td className="text-end">
                     <button
@@ -46,15 +89,31 @@ export const Table = ({ taskList, switchTask, handleOnDelete }) => {
         <div className="row mt-5">
           <div className="col">
             <h3 className="text-center">Bad List</h3>
-
             <hr />
+            <input
+              className="form-check-input"
+              type="checkbox"
+              value="all-bad"
+              id="all-bad"
+              onChange={handleOnSelect}
+            />{" "}
+            <label htmlFor="all-bad">Select All</label>
             <table className="table table-striped table-hover border">
               <tbody id="badList">
                 {badList.map((item, i) => {
                   return (
-                    <tr key={item._id}>
+                    <tr key={item?._id}>
                       <td>{i + 1}</td>
-                      <td>{item.task}</td>
+                      <td>
+                        {" "}
+                        <input
+                          className="form-check-input"
+                          type="checkbox"
+                          value={item?._id}
+                          onChange={handleOnSelect}
+                        />{" "}
+                        {item.task}
+                      </td>
                       <td>{item.hr}</td>
                       <td className="text-end">
                         <button
@@ -75,7 +134,6 @@ export const Table = ({ taskList, switchTask, handleOnDelete }) => {
                 })}
               </tbody>
             </table>
-
             <div className="alert alert-success">
               You could have saved ={" "}
               <span id="savedHrsElm">
