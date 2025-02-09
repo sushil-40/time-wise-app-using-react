04c07d76c089284b2a5fb257020cd346633ecede
Sushil <grayson.9999@gmail.com>
2025-02-09 13:43:24 +1000

    adding delete button upon selecing check box

--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -42,121 +42,128 @@ export const Table = ({ taskList, switchTask, handleOnDelete }) => {
   };
   console.log(toDelete);
   return (
-    <div className="row mt-5">
-      <div className="col">
-        <h3 className="text-center">Entry List</h3>
-        {/* <!-- Entry List table  --> */}
-        <hr />
-        <input
-          className="form-check-input"
-          type="checkbox"
-          value="all-entry"
-          id="all-entry"
-          onChange={handleOnSelect}
-        />{" "}
-        <label htmlFor="all-entry">Select All</label>
-        <table className="table table-striped table-hover border">
-          <tbody id="entryList">
-            {entryList.map((item, i) => {
-              return (
-                <tr key={item?._id}>
-                  <td>{i + 1}</td>
-                  <td>
-                    {" "}
-                    <input
-                      className="form-check-input"
-                      type="checkbox"
-                      value={item?._id}
-                      onChange={handleOnSelect}
-                      checked={toDelete.includes(item?._id)}
-                    />{" "}
-                    {item.task}
-                  </td>
-                  <td>{item.hr}</td>
-                  <td className="text-end">
-                    <button
-                      className="btn btn-danger"
-                      onClick={() => handleOnDelete(item._id)}
-                    >
-                      <i className="fa-solid fa-trash"></i>
-                    </button>
-                    <button
-                      onClick={() => switchTask(item._id, "bad")}
-                      className="btn btn-success"
-                    >
-                      <i className="fa-solid fa-arrow-right"></i>
-                    </button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </table>
-      </div>
+    <>
+      <div className="row mt-5">
+        <div className="col">
+          <h3 className="text-center">Entry List</h3>
+          {/* <!-- Entry List table  --> */}
+          <hr />
+          <input
+            className="form-check-input"
+            type="checkbox"
+            value="all-entry"
+            id="all-entry"
+            onChange={handleOnSelect}
+          />{" "}
+          <label htmlFor="all-entry">Select All</label>
+          <table className="table table-striped table-hover border">
+            <tbody id="entryList">
+              {entryList.map((item, i) => {
+                return (
+                  <tr key={item?._id}>
+                    <td>{i + 1}</td>
+                    <td>
+                      {" "}
+                      <input
+                        className="form-check-input"
+                        type="checkbox"
+                        value={item?._id}
+                        onChange={handleOnSelect}
+                        checked={toDelete.includes(item?._id)}
+                      />{" "}
+                      {item.task}
+                    </td>
+                    <td>{item.hr}</td>
+                    <td className="text-end">
+                      <button
+                        className="btn btn-danger"
+                        onClick={() => handleOnDelete(item._id)}
+                      >
+                        <i className="fa-solid fa-trash"></i>
+                      </button>
+                      <button
+                        onClick={() => switchTask(item._id, "bad")}
+                        className="btn btn-success"
+                      >
+                        <i className="fa-solid fa-arrow-right"></i>
+                      </button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </table>
+        </div>
 
-      <div className="col bg-info">
-        {/* <!-- Bad List table  --> */}
+        <div className="col bg-info">
+          {/* <!-- Bad List table  --> */}
 
-        <div className="row mt-5">
-          <div className="col">
-            <h3 className="text-center">Bad List</h3>
-            <hr />
-            <input
-              className="form-check-input"
-              type="checkbox"
-              value="all-bad"
-              id="all-bad"
-              onChange={handleOnSelect}
-            />{" "}
-            <label htmlFor="all-bad">Select All</label>
-            <table className="table table-striped table-hover border">
-              <tbody id="badList">
-                {badList.map((item, i) => {
-                  return (
-                    <tr key={item?._id}>
-                      <td>{i + 1}</td>
-                      <td>
-                        {" "}
-                        <input
-                          className="form-check-input"
-                          type="checkbox"
-                          value={item?._id}
-                          onChange={handleOnSelect}
-                          checked={toDelete.includes(item?._id)}
-                        />{" "}
-                        {item.task}
-                      </td>
-                      <td>{item.hr}</td>
-                      <td className="text-end">
-                        <button
-                          onClick={() => switchTask(item._id, "entry")}
-                          className="btn btn-warning"
-                        >
-                          <i className="fa-solid fa-arrow-left"></i>
-                        </button>
-                        <button
-                          className="btn btn-danger"
-                          onClick={() => handleOnDelete(item.id)}
-                        >
-                          <i className="fa-solid fa-trash"></i>
-                        </button>
-                      </td>
-                    </tr>
-                  );
-                })}
-              </tbody>
-            </table>
-            <div className="alert alert-success">
-              You could have saved ={" "}
-              <span id="savedHrsElm">
+          <div className="row mt-5">
+            <div className="col">
+              <h3 className="text-center">Bad List</h3>
+              <hr />
+              <input
+                className="form-check-input"
+                type="checkbox"
+                value="all-bad"
+                id="all-bad"
+                onChange={handleOnSelect}
+              />{" "}
+              <label htmlFor="all-bad">Select All</label>
+              <table className="table table-striped table-hover border">
+                <tbody id="badList">
+                  {badList.map((item, i) => {
+                    return (
+                      <tr key={item?._id}>
+                        <td>{i + 1}</td>
+                        <td>
+                          {" "}
+                          <input
+                            className="form-check-input"
+                            type="checkbox"
+                            value={item?._id}
+                            onChange={handleOnSelect}
+                            checked={toDelete.includes(item?._id)}
+                          />{" "}
+                          {item.task}
+                        </td>
+                        <td>{item.hr}</td>
+                        <td className="text-end">
+                          <button
+                            onClick={() => switchTask(item._id, "entry")}
+                            className="btn btn-warning"
+                          >
+                            <i className="fa-solid fa-arrow-left"></i>
+                          </button>
+                          <button
+                            className="btn btn-danger"
+                            onClick={() => handleOnDelete(item.id)}
+                          >
+                            <i className="fa-solid fa-trash"></i>
+                          </button>
+                        </td>
+                      </tr>
+                    );
+                  })}
+                </tbody>
+              </table>
+              <div className="alert alert-success">
+                You could have saved ={" "}
                 <span id="savedHrsElm">
-                  {badList.reduce((acc, item) => acc + Number(item.hr), 0)}
+                  <span id="savedHrsElm">
+                    {badList.reduce((acc, item) => acc + Number(item.hr), 0)}
+                  </span>
                 </span>
-              </span>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {toDelete.length > 0 && (
+        <div className="row my-5 d-grid">
+          <div className="btn btn-danger">Delete {toDelete.length} task(s)</div>
+        </div>
+      )}
+    </>
   );
 };
