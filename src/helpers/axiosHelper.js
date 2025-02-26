8be43d44b9f693fb834074ca55851202c7865e1a
Sushil <grayson.9999@gmail.com>
2025-02-26 20:11:01 +1000

    adding dependecies

--- src/helpers/axiosHelper.js
+++ src/helpers/axiosHelper.js
@@ -1,5 +1,8 @@
 import axios from "axios";
-const apiEP = "http://localhost:8000/api/v1/tasks";
+const apiEP = import.meta.env.NODE_ENV
+  ? "/api/v1/tasks"
+  : "http://localhost:8000/api/v1/tasks";
+
 const apiProcessor = async ({ method, data }) => {
   try {
     const response = await axios({
